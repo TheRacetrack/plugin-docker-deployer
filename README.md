f92317fc3cd331fb3d8b7cd98e94005d8816f1ae
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2022-12-06 14:27:15 +0100

    Add stub for docker deployer plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Racetrack Plugin: Docker Deployer
+
+A Racetrack plugin allowing to deploy services to a local Docker
---
+++ src/plugin.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import Any
+
+from racetrack_client.log.logs import get_logger
+
+logger = get_logger(__name__)
+
+try:
+    from lifecycle.deployer.infra_target import InfrastructureTarget
+    from deployer import DockerFatmanDeployer
+    from monitor import DockerMonitor
+    from logs_streamer import DockerLogsStreamer
+except ModuleNotFoundError:
+    logger.debug('Skipping Lifecycle\'s imports')
+
+
+class Plugin:
+
+    def infrastructure_targets(self) -> dict[str, Any]:
+        """
+        Infrastracture Targets (deployment targets) for Fatmen provided by this plugin
+        :return dict of infrastructure name -> an instance of lifecycle.deployer.infra_target.InfrastructureTarget
+        """
+        return {
+            'docker': InfrastructureTarget(
+                'docker',
+                fatman_deployer=DockerFatmanDeployer(),
+                fatman_monitor=DockerMonitor(),
+                logs_streamer=DockerLogsStreamer(),
+            ),
+        }
