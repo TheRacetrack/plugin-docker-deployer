9c5b017590b0c3ec6a9e27250ad58369bad08a3d
Thor K <thorkik@gmail.com>
2023-02-14 10:38:56 +0100

    Renaming fatman to job (#2)
    * Degreasing plugin
    
    * Version bump

--- src/plugin.py
+++ src/plugin.py
@@ -6,7 +6,7 @@ from racetrack_client.log.logs import get_logger
 
 if 'lifecycle' in sys.modules:
     from lifecycle.deployer.infra_target import InfrastructureTarget
-    from deployer import DockerFatmanDeployer
+    from deployer import DockerJobDeployer
     from monitor import DockerMonitor
     from logs_streamer import DockerLogsStreamer
 
@@ -17,13 +17,13 @@ class Plugin:
 
     def infrastructure_targets(self) -> dict[str, Any]:
         """
-        Infrastructure Targets (deployment targets) for Fatmen provided by this plugin
+        Infrastructure Targets (deployment targets) for Jobs provided by this plugin
         :return dict of infrastructure name -> an instance of lifecycle.deployer.infra_target.InfrastructureTarget
         """
         return {
             'docker': InfrastructureTarget(
-                fatman_deployer=DockerFatmanDeployer(),
-                fatman_monitor=DockerMonitor(),
+                job_deployer=DockerJobDeployer(),
+                job_monitor=DockerMonitor(),
                 logs_streamer=DockerLogsStreamer(),
             ),
         }
